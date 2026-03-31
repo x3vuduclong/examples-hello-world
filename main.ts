05edb4dd0b021cf62187fcfc2764a99460639b39
x3vuduclong <vuduclong2003@gmail.com>
2026-03-31 16:50:51 +0700

    Test proxy server
    Updated Deno server to proxy requests to Facebook.

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
-});
+// Deno.serve((req: Request) => {
+//     return serveFile(req, "./index.html");
+// });
+
+Deno.serve({ port: 80 }, async (request) => {
+  const { pathname, search } = new URL(request.url)
+  const url = new URL('.' + pathname, 'https://facebook.com')
+  url.search = search
+
+  const headers = new Headers(request.headers)
+  headers.set('Host', url.hostname)
+  // headers.set('Authorization', Deno.env.get('PROXY_AUTHORIZATION'))
+
+  return fetch(url, {
+    method: request.method,
+    headers,
+    body: request.body,
+    redirect: 'manual',
+  })
+})
